ccfe11d0216c7c6881ebfec11e0e15b47decfb33
abdelrahman-abied <abied_2011@yahoo.com>
2020-02-12 21:39:27 +0200

    convert observer to lambda expression

--- app/src/main/java/com/kira/facebookapplication/ui/PostViewModel.java
+++ app/src/main/java/com/kira/facebookapplication/ui/PostViewModel.java
@@ -1,5 +1,6 @@
 package com.kira.facebookapplication.ui;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
@@ -22,33 +23,36 @@ public class PostViewModel extends ViewModel {
     //    MutableLiveData<String> posts = new MutableLiveData<>();
     private static final String TAG = "PostViewModel";
 
+    @SuppressLint("CheckResult")
     public void getPosts() {
-        Observable observable = PostClient.getINSTANCE().getPosts()
+        Observable<List<PostModel>> observable = PostClient.getINSTANCE().getPosts()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io());
 
 
-        Observer<List<PostModel>> observer = new Observer<List<PostModel>>() {
-            @Override
-            public void onSubscribe(Disposable d) {
+//        Observer<List<PostModel>> observer = new Observer<List<PostModel>>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(List<PostModel> postModels) {
+//                postsMutableLiveData.setValue(postModels);
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.d(TAG, "onError: " + e);
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        };
+//        observable.subscribe(observer);
 
-            }
-
-            @Override
-            public void onNext(List<PostModel> postModels) {
-                postsMutableLiveData.setValue(postModels);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError: " + e);
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        };
-        observable.subscribe(observer);
+observable.subscribe(o -> postsMutableLiveData.setValue(o),e-> Log.d(TAG, "getPostError: "+e));
     }
 }
